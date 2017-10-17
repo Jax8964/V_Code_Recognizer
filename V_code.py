0bfd6b744e14b61d79b82aedd1e27f3b58609680
Steve-Wjh <3160100748@zju.edu.cn>
2017-10-17 17:01:05 +0800

    creating V_code.py

---
+++ V_code.py
@@ -0,0 +1,11 @@
+"This is a program using tensorflow to recognize verification codes that contains only numbers and alphabets."
+
+"Created by Steve Wang and Jax Sun"
+
+"the code begins below the line:"
+"-----------------------------------------------------------------------------------------------------"
+
+"import python modules"
+import tensorflow as ts   
+
+
